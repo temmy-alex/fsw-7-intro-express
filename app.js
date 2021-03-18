057bdd425571bee2b739630e23d06cc262bd75e1
Temmy Alex <temmy.alex@gmail.com>
2021-03-19 00:53:04 +0700

    Update foreach ejs

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
 var express = require('express');
 var app = express();
-
+var bcrypt = require('bcrypt');
 // Set template engine yang akan digunakan
 app.set('view engine', 'ejs');
+
+app.use('/assets', express.static('assets'));
+app.use(express.urlencoded({ extended: true }));
 // Buat folder views
 
-// Response string
+const cpuInfo = require('./data/cpu.json');
+const getCpu = cpuInfo.dataCPU;
+
+var salt = bcrypt.genSaltSync(10);
+var test = 'test';
+console.log(bcrypt.hashSync(test, salt));
+// Mas Temmy
+app.get('/datacpu', (req, res) => {
+    res.render('datacpu', {dataCPU: getCpu})
+})
+
+app.get('/datacpuCasey', (req, res) => {
+    res.render('datacpuCasey', {dataCPU: getCpu})
+})
+
+app.get('/dataspeedIlham', (req, res) => {
+    res.render('ilham', {dataCPU: getCpu})
+})
+
+app.get('/dataspeedFateh', (req, res) => {
+    res.render('timesfateh', {dataCPU: getCpu})
+})
+
+// Reponse string
 // app.get('/', (req, res) => {
 //     res.send('Hello World');
 // });
@@ -74,11 +100,22 @@ app.get('/list-student', (req, res) => {
     res.render('liststudent', {text, murid: students});
 })
 
+app.get('/form', (req, res) => {
+    const notif = req.query.notif
+    res.render('form', {notif});
+})
+
+app.post('/form', (req, res) => {
+    res.json(email);
+})
+
 // http://localhost:3000/home?notif=sukses
 // Jika terdapat query string notif dengan nilai sukses maka akan muncul alert bootstrap success
 app.get('/home', (req, res) => {
     const notif = req.query.notif
     res.render('home', {notif_success: notif});
 })
+
+
 
 app.listen(3000);
